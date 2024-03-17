b487aa1108835381d9e1da9d78d8b5246d5bb1cb
Peter Dinis <peterdinis0@pobox.sk>
2024-03-17 14:37:53 +0100

    feat: Working on QuestionsCards WIP

--- app/_components/questions/HomepageQuestions.tsx
+++ app/_components/questions/HomepageQuestions.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import QuestionSearch from './QuestionSearch';
+import QuestionsCards from './QuestionsCards';
 
 const HomepageQuestions: FC = () => {
     return (
         <>
             <QuestionSearch />
+            <QuestionsCards />
         </>
     );
 };
---
+++ app/_components/questions/QuestionsCards.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const QuestionsCards: FC = () => {
+    return (
+        <section className="mt-6 ml-4 p-2 flex justify-center align-top">
+            CARDS
+        </section>
+    )
+}
+
+export default QuestionsCards;
