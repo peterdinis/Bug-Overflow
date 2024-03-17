1fb4e8aa4504d2f6b04ae7fd1acd6bd35acab774
Peter Dinis <peterdinis0@pobox.sk>
2024-03-17 16:42:34 +0100

    feat: Updating sidebar

--- app/_components/shared/Sidebar.tsx
+++ app/_components/shared/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
-
+import { FaQuestionCircle } from "react-icons/fa";
+import { FaPersonCircleQuestion } from "react-icons/fa6";
 const Sidebar: FC = () => {
     return (
         <>
@@ -13,122 +14,14 @@ const Sidebar: FC = () => {
                     <ul className='mt-12'>
                         <li className='flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6'>
                             <div className='flex items-center'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='icon icon-tabler icon-tabler-grid'
-                                    width={18}
-                                    height={18}
-                                    viewBox='0 0 24 24'
-                                    strokeWidth='1.5'
-                                    stroke='currentColor'
-                                    fill='none'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                >
-                                    <path stroke='none' d='M0 0h24v24H0z' />
-                                    <rect
-                                        x={4}
-                                        y={4}
-                                        width={6}
-                                        height={6}
-                                        rx={1}
-                                    />
-                                    <rect
-                                        x={14}
-                                        y={4}
-                                        width={6}
-                                        height={6}
-                                        rx={1}
-                                    />
-                                    <rect
-                                        x={4}
-                                        y={14}
-                                        width={6}
-                                        height={6}
-                                        rx={1}
-                                    />
-                                    <rect
-                                        x={14}
-                                        y={14}
-                                        width={6}
-                                        height={6}
-                                        rx={1}
-                                    />
-                                </svg>
-                                <span className='text-sm  ml-2'>Dashboard</span>
-                            </div>
-                            <div className='py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs'>
-                                5
+                                <FaQuestionCircle />
+                                <span className='text-sm  ml-2'>All questions</span>
                             </div>
                         </li>
                         <li className='flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6'>
                             <div className='flex items-center'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='icon icon-tabler icon-tabler-puzzle'
-                                    width={18}
-                                    height={18}
-                                    viewBox='0 0 24 24'
-                                    strokeWidth='1.5'
-                                    stroke='currentColor'
-                                    fill='none'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                >
-                                    <path stroke='none' d='M0 0h24v24H0z' />
-                                    <path d='M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1' />
-                                </svg>
-                                <span className='text-sm  ml-2'>Products</span>
-                            </div>
-                            <div className='py-1 px-3 bg-gray-700 rounded text-gray-500 flex items-center justify-center text-xs'>
-                                8
-                            </div>
-                        </li>
-                        <li className='flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6'>
-                            <div className='flex items-center'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='icon icon-tabler icon-tabler-compass'
-                                    width={18}
-                                    height={18}
-                                    viewBox='0 0 24 24'
-                                    strokeWidth='1.5'
-                                    stroke='currentColor'
-                                    fill='none'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                >
-                                    <path stroke='none' d='M0 0h24v24H0z' />
-                                    <polyline points='8 16 10 10 16 8 14 14 8 16' />
-                                    <circle cx={12} cy={12} r={9} />
-                                </svg>
-                                <span className='text-sm  ml-2'>
-                                    Performance
-                                </span>
-                            </div>
-                        </li>
-                        <li className='flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6'>
-                            <div className='flex items-center'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg'
-                                    className='icon icon-tabler icon-tabler-code'
-                                    width={20}
-                                    height={20}
-                                    viewBox='0 0 24 24'
-                                    strokeWidth='1.5'
-                                    stroke='currentColor'
-                                    fill='none'
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                >
-                                    <path stroke='none' d='M0 0h24v24H0z' />
-                                    <polyline points='7 8 3 12 7 16' />
-                                    <polyline points='17 8 21 12 17 16' />
-                                    <line x1={14} y1={4} x2={10} y2={20} />
-                                </svg>
-                                <span className='text-sm  ml-2'>
-                                    Deliverables
-                                </span>
+                                <FaPersonCircleQuestion />
+                                <span className='text-sm  ml-2'>My Questions</span>
                             </div>
                         </li>
                         <li className='flex w-full justify-between text-gray-600 hover:text-gray-500 cursor-pointer items-center mb-6'>
