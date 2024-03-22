1bd2f277dd9ba75c48aba413ba636ba8f6de1d8e
Peter Dinis <peterdinis0@pobox.sk>
2024-03-22 21:12:54 +0100

    feat: Working on technologies WIP

--- app/_components/shared/sidebar/Sidebar.tsx
+++ app/_components/shared/sidebar/Sidebar.tsx
@@ -8,123 +8,138 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
 } from '@/components/ui/tooltip';
 
 const Sidebar: FC = () => {
-  const [collapsed, setSidebarCollapsed] = useState(false);
-  const { toast } = useToast();
-  const router = useRouter();
+    const [collapsed, setSidebarCollapsed] = useState(false);
+    const { toast } = useToast();
+    const router = useRouter();
 
-  const logoutUser = () => {
-    toast({
-      title: 'Logout was successfull',
-      className: 'bg-green-400',
-      duration: 2000,
-    });
-    router.push('/login');
-  };
+    const logoutUser = () => {
+        toast({
+            title: 'Logout was successfull',
+            className: 'bg-green-400',
+            duration: 2000,
+        });
+        router.push('/login');
+    };
 
-  return (
-    <div
-      className={classNames({
-        'grid min-h-screen': true,
-        'grid-cols-sidebar': !collapsed,
-        'grid-cols-sidebar-collapsed': collapsed,
-        'transition-[grid-template-columns] duration-300 ease-in-out': true,
-      })}
-    >
-      <div className='bg-gray-100 text-black'>
-        <button onClick={() => setSidebarCollapsed((prev) => !prev)}>
-          {collapsed === true ? (
-            <Menu className='w-7 h-7' />
-          ) : (
-            <XCircle className='w-7 h-7' />
-          )}
-        </button>
-        {collapsed === false ? (
-          <>
-            <div>
-              <div className='mt-8'>
-                <div className='mt-8'>
-                  <Button variant={'ghost'} value='sm' onClick={logoutUser}>
-                    <LogOut onClick={logoutUser} />
-                    Logout
-                  </Button>
-                </div>{' '}
-              </div>
+    return (
+        <div
+            className={classNames({
+                'grid min-h-screen': true,
+                'grid-cols-sidebar': !collapsed,
+                'grid-cols-sidebar-collapsed': collapsed,
+                'transition-[grid-template-columns] duration-300 ease-in-out':
+                    true,
+            })}
+        >
+            <div className='bg-gray-100 text-black'>
+                <button onClick={() => setSidebarCollapsed((prev) => !prev)}>
+                    {collapsed === true ? (
+                        <Menu className='w-7 h-7' />
+                    ) : (
+                        <XCircle className='w-7 h-7' />
+                    )}
+                </button>
+                {collapsed === false ? (
+                    <>
+                        <div>
+                            <div className='mt-8'>
+                                <div className='mt-8'>
+                                    <Button
+                                        variant={'ghost'}
+                                        value='sm'
+                                        onClick={logoutUser}
+                                    >
+                                        <LogOut onClick={logoutUser} />
+                                        Logout
+                                    </Button>
+                                </div>{' '}
+                            </div>
 
-              <div className='mt-8'>
-                <Button variant={'ghost'} value='sm'>
-                  <Upload />
-                  <Link href='/upload'>Upload new file</Link>
-                </Button>
-              </div>
+                            <div className='mt-8'>
+                                <Button variant={'ghost'} value='sm'>
+                                    <Upload />
+                                    <Link href='/upload'>Upload new file</Link>
+                                </Button>
+                            </div>
 
-              <div className='mt-8'>
-                <Button variant={'ghost'} value='sm'>
-                  <Files />
-                  <Link href='/files'>My all files</Link>
-                </Button>
-              </div>
+                            <div className='mt-8'>
+                                <Button variant={'ghost'} value='sm'>
+                                    <Files />
+                                    <Link href='/files'>My all files</Link>
+                                </Button>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div>
+                        <div className='mt-8'>
+                            <div className='mt-8'>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <Button
+                                                onClick={logoutUser}
+                                                variant={'ghost'}
+                                                size={'sm'}
+                                            >
+                                                <LogOut />
+                                            </Button>
+                                        </TooltipTrigger>
+                                        <TooltipContent>Logout</TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                            </div>
+                            <div className='mt-8'>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <Button
+                                                variant={'ghost'}
+                                                size={'sm'}
+                                            >
+                                                <Link href='/upload'>
+                                                    <Upload />
+                                                </Link>
+                                                <TooltipContent>
+                                                    Upload file
+                                                </TooltipContent>
+                                            </Button>
+                                        </TooltipTrigger>
+                                    </Tooltip>
+                                </TooltipProvider>
+                            </div>
+                            <div className='mt-8'>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <Button
+                                                variant={'ghost'}
+                                                size={'sm'}
+                                            >
+                                                <Link href='/files'>
+                                                    <Files />
+                                                </Link>
+                                                <TooltipContent>
+                                                    My files
+                                                </TooltipContent>
+                                            </Button>
+                                        </TooltipTrigger>
+                                    </Tooltip>
+                                </TooltipProvider>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
-          </>
-        ) : (
-          <div>
-            <div className='mt-8'>
-              <div className='mt-8'>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button
-                        onClick={logoutUser}
-                        variant={'ghost'}
-                        size={'sm'}
-                      >
-                        <LogOut />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>Logout</TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-              <div className='mt-8'>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant={'ghost'} size={'sm'}>
-                        <Link href='/upload'>
-                          <Upload />
-                        </Link>
-                        <TooltipContent>Upload file</TooltipContent>
-                      </Button>
-                    </TooltipTrigger>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-              <div className='mt-8'>
-                <TooltipProvider>
-                  <Tooltip>
-                    <TooltipTrigger>
-                      <Button variant={'ghost'} size={'sm'}>
-                        <Link href='/files'>
-                          <Files />
-                        </Link>
-                        <TooltipContent>My files</TooltipContent>
-                      </Button>
-                    </TooltipTrigger>
-                  </Tooltip>
-                </TooltipProvider>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Sidebar;
--- app/_components/technologies/TechnologiesWrapper.tsx
+++ app/_components/technologies/TechnologiesWrapper.tsx
@@ -5,6 +5,7 @@ import Header from '../shared/Header';
 import Sidebar from '../shared/sidebar/Sidebar';
 import { useQuery } from '@apollo/client';
 import { GET_ALL_TECHNOLOGIES } from '@/app/_graphql/queries/technologyQueries';
+import TechnologiesSearch from './TechnologiesSearch';
 
 const TechnologiesWrapper: FC = () => {
     const { data, error, loading } = useQuery(GET_ALL_TECHNOLOGIES);
@@ -19,6 +20,9 @@ const TechnologiesWrapper: FC = () => {
             <div className='container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6'>
                 <div className='w-full h-full rounded '>
                     <Header text='Technologies' />
+                    <div className='mt-5'>
+                        <TechnologiesSearch />
+                    </div>
                     <section className='mt-4'>rrrr</section>
                 </div>
             </div>
