12fae8289c0fae76eea7e47cd88c539ef4fb2f92
Peter Dinis <peterdinis0@pobox.sk>
2024-03-23 09:16:20 +0100

    feat: Add new button

--- app/_components/technologies/TechnologiesCards.tsx
+++ app/_components/technologies/TechnologiesCards.tsx
@@ -1,5 +1,7 @@
 import { GetAllTechnologies } from '@/app/_graphql/types/TechnologyTypes';
 import { FC } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const TechnologiesCards: FC<GetAllTechnologies> = ({
     name,
@@ -12,12 +14,17 @@ const TechnologiesCards: FC<GetAllTechnologies> = ({
                     <img
                         src={image}
                         alt={name}
-                        className='h-80 w-72 object-cover rounded-t-xl'
+                        className='h-50 w-50 object-cover rounded-t-xl'
                     />
                     <div className='px-4 py-3 w-72'>
                         <span className='text-gray-400 mr-3 uppercase text-xs'>
                             {name}
                         </span>
+                        <Link href="/">
+                        <Button variant={"ghost"} size={"lg"}>
+                            Find all {name} questions
+                        </Button>
+                    </Link>
                     </div>
                 </span>
             </div>
